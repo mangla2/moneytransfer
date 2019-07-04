7adf1dedce3b2b01ee3e1425e346230d06f02937
Shubham Mangla <manglashubham94@gmail.com>
2019-07-04 16:47:44 +0530

    Maven project created

---
+++ src/main/java/com/revolut/app/App.java
@@ -0,0 +1,13 @@
+package com.revolut.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
